277ef793685efccf1ff398d2bc9d3a7b44e21f0f
Maciej Futyma <mfutyma2000@gmail.com>
2023-11-15 18:59:54 +0100

    create players panel

---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export const Player = ({ name, symbol }) => {
+    const [isEdited, setIsEdited] = useState(false)
+    const [playerName, setPlayerName] = useState(name)
+
+    const handleEditClick = () => {
+        setIsEdited(editing => !editing)
+    }
+
+    const handleNameChange = (event) => {
+        setPlayerName(event.target.value)
+    }
+
+    let playerNameElement = <span className="player-name">{playerName}</span>
+
+    if(isEdited){
+        playerNameElement = <input type="text" required value={playerName} onChange={handleNameChange}/>
+    }
+
+  return (
+    <li>
+      <span className="player">
+        {playerNameElement}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEdited ? "Save" : "Edit"}</button>
+    </li>
+  );
+};
